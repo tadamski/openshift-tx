cebf8150cf10ce1d64c66275587203f617029d34
Tomasz Adamski <tadamski@redhat.com>
2019-03-05 22:20:25 +0100

    Node name debug resource

---
+++ tx-server/src/main/java/org/jboss/as/quickstarts/xa/server/NodeNameResource.java
@@ -0,0 +1,17 @@
+package org.jboss.as.quickstarts.xa.server;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+public class NodeNameResource {
+
+    @GET
+    @Path("node")
+    @Produces("text/plain")
+    public String node() {
+        return System.getProperty("jboss.node.name");
+    }
+
+}
