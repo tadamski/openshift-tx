26e0136cc7470af3dac6931af32714a8ee337b97
Ondra Chaloupka <ochaloup@redhat.com>
2019-03-07 15:49:01 +0100

    adding ability for mock resource to be persistent

---
+++ tx-server/src/main/java/org/jboss/as/quickstarts/xa/server/resources/MockXAResourceInitializer.java
@@ -0,0 +1,47 @@
+package org.jboss.as.quickstarts.xa.server.resources;
+
+import java.util.Vector;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import org.jboss.logging.Logger;
+
+import com.arjuna.ats.arjuna.recovery.RecoveryManager;
+import com.arjuna.ats.arjuna.recovery.RecoveryModule;
+import com.arjuna.ats.internal.jta.recovery.arjunacore.XARecoveryModule;
+
+@Singleton
+@Startup
+public class MockXAResourceInitializer {
+    private static Logger log = Logger.getLogger(MockXAResource.class);
+
+    /**
+     * register the recovery module with the transaction manager.
+     */
+    @PostConstruct
+    public void postConstruct() {
+        log.info("TestXAResourceRecoveryHelper starting");
+        getRecoveryModule().addXAResourceRecoveryHelper(MockXAResourceRecoveryHelper.INSTANCE);
+    }
+
+    /**
+     * unregister the recovery module from the transaction manager.
+     */
+    @PreDestroy
+    public void preDestroy() {
+        log.info("TestXAResourceRecoveryHelper stopping");
+        getRecoveryModule().removeXAResourceRecoveryHelper(MockXAResourceRecoveryHelper.INSTANCE);
+    }
+
+    private XARecoveryModule getRecoveryModule() {
+        for (RecoveryModule recoveryModule : ((Vector<RecoveryModule>) RecoveryManager.manager().getModules())) {
+            if (recoveryModule instanceof XARecoveryModule) {
+                return (XARecoveryModule) recoveryModule;
+            }
+        }
+        return null;
+    }
+}
---
+++ tx-server/src/main/java/org/jboss/as/quickstarts/xa/server/resources/MockXAResourceRecoveryHelper.java
@@ -0,0 +1,28 @@
+package org.jboss.as.quickstarts.xa.server.resources;
+
+import javax.transaction.xa.XAResource;
+
+import com.arjuna.ats.jta.recovery.XAResourceRecoveryHelper;
+
+public class MockXAResourceRecoveryHelper implements XAResourceRecoveryHelper {
+    public static final MockXAResourceRecoveryHelper INSTANCE = new MockXAResourceRecoveryHelper();
+    private static final MockXAResource mockXARecoveringInstance = new MockXAResource();
+
+    private MockXAResourceRecoveryHelper() {
+        if(INSTANCE != null) {
+            throw new IllegalStateException("singleton instance can't be instantiated twice");
+        }
+    }
+
+    @Override
+    public boolean initialise(String p) throws Exception {
+        MockXAResource.initPreparedXids(MockXAResourceStorage.recoverFromDisk());
+        return true;
+    }
+
+    @Override
+    public XAResource[] getXAResources() throws Exception {
+        return new XAResource[] { mockXARecoveringInstance };
+    }
+
+}
