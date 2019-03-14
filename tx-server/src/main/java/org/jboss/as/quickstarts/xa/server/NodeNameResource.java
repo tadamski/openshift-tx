cf2bc5fabc0951923d138e5bbf5b368023ebf159
Tomasz Adamski <tadamski@redhat.com>
2019-03-14 14:58:06 +0100

    REMOVEME StatefulSet DNS readiness test

--- tx-server/src/main/java/org/jboss/as/quickstarts/xa/server/NodeNameResource.java
+++ tx-server/src/main/java/org/jboss/as/quickstarts/xa/server/NodeNameResource.java
@@ -3,6 +3,8 @@ package org.jboss.as.quickstarts.xa.server;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class NodeNameResource {
@@ -12,6 +14,14 @@ public class NodeNameResource {
     @Produces("text/plain")
     public String node() {
         return System.getProperty("jboss.node.name");
+    }
+
+    @GET
+    @Path("ready")
+    @Produces("text/plain")
+    public Response ready(){
+        String node = node();
+        return Integer.parseInt(node.substring(node.length()-1))%2 == 0 ? Response.ok(node, MediaType.TEXT_PLAIN_TYPE).build() : Response.status(Response.Status.NOT_FOUND).build();
     }
 
 }
